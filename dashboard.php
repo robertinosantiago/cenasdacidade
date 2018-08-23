96c72670bad7ada052492d6e7385eb27a1471dd0
Robertino Mendes Santiago Junior <robertino@ufpr.br>
2018-08-23 09:47:14 -0300

    atualizações

--- dashboard.php
+++ dashboard.php
@@ -12,14 +12,31 @@ $twig = new Twig_Environment($loader, array(
     // 'cache' => __DIR__ . '/cache',
 ));
 
+$toast = null;
+
 $storage = new Session();
 $user = $storage->get('user');
+
+if(isset($_GET['e'])) {
+  $e = $_GET['e'];
+  switch ($e) {
+    case 1:
+      $toast = array('type' => 'error', 'message' => 'Operação inválida.');
+      break;
+    case 2:
+      $toast = array('type' => 'error', 'message' => 'Você não tem permissão para excluir essa foto.');
+      break;
+    case 3:
+      $toast = array('type' => 'success', 'message' => 'Foto removida com sucesso.');
+      break;
+  }
+}
 
 if ($user) {
   $loggedIn = $storage->get('loggedIn');
   $photos = PhotoDao::getInstance()->findByUserId($user['User']['id']);
 
-  echo $twig->render('dashboard.html', array('user' => $user, 'photos' => $photos, 'loggedIn' => $loggedIn));
+  echo $twig->render('dashboard.html', array('toast' => $toast, 'user' => $user, 'photos' => $photos, 'loggedIn' => $loggedIn));
 } else {
   $toast = array('type' => 'info', 'message' => 'Por favor, acesse sua conta.');
   echo $twig->render('login.html', array('toast' => $toast));
--- delete.php
+++ delete.php
@@ -10,24 +10,20 @@ $storage = new Session();
 $user = $storage->get('user');
 
 if (!$user) {
-  // TODO: Deve estar logado
-  HttpClient\Util::redirect('login.php');
+  HttpClient\Util::redirect('login.php?e=1');
 }
 
 if (!$_POST) {
-  // TODO: operação inválida
-  HttpClient\Util::redirect('dashboard.php');
+  HttpClient\Util::redirect('dashboard.php?e=1');
 }
 
 $photo = PhotoDao::getInstance()->findById($_POST['photo']);
 
 if ($photo->getUserId() != $user['User']['id']) {
-  // TODO: você não tem permissão para excluir essa foto
-  HttpClient\Util::redirect('dashboard.php');
+  HttpClient\Util::redirect('dashboard.php?e=2');
 }
 
 if (PhotoDao::getInstance()->delete($photo->getId())) {
   unlink(realpath('photos/'.$photo->getPath()));
-  // TODO: removido com sucesso
-  HttpClient\Util::redirect('dashboard.php');
+  HttpClient\Util::redirect('dashboard.php?e=3');
 }
